7e2960330db2c6dceab6780b18740bdfbb09f7f5
neillane-carvalho <neillane.rosario@ucsal.edu.br>
2024-10-31 13:56:11 -0300

    0.0.1

---
+++ src/main/java/ucsal/gestaoaluno/GestaoalunoApplication.java
@@ -0,0 +1,13 @@
+package ucsal.gestaoaluno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoalunoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestaoalunoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ucsal/gestaoaluno/GestaoalunoApplicationTests.java
@@ -0,0 +1,13 @@
+package ucsal.gestaoaluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoalunoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
